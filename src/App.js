102f9d317e552d2819d33d46b906b8bff8829eb5
rokkuminchan <loc@world-works.co.jp>
2020-12-29 14:50:52 +0900

    Add SignIn and SignUp pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import SignIn from './components/pages/SignIn';
+import SignUp from './components/pages/SignUp';
+import ScrollToTop from './components/common/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ScrollToTop>
+          <Switch>
+            <Route exact path="/">
+              <div>Welcome home</div>
+            </Route>
+            <Route exact path="/signin">
+              <SignIn />
+            </Route>
+            <Route exact path="/signup">
+              <SignUp />
+            </Route>
+          </Switch>
+        </ScrollToTop>
+      </Router>
     </div>
   );
 }
